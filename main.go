ef66d580ec1a7206e89819a38f789758d2cda726
unknown <mxh15715247587@gmail.com>
2024-06-20 10:31:40 +0800

    20240620:1031

--- main.go
+++ main.go
@@ -1,33 +1,30 @@
 package main
 
 import (
-	"fmt"
-
-	blockchain "github.com/icfoxy/GoBlockChain/BlockChain"
+	chainserver "github.com/icfoxy/GoBlockChain/ChainServer"
 )
 
 func main() {
-	// server := NewServer("localhost:", 8088)
-	// server.AddHandlerFunc("/TestAlive", TestAliveHandler)
-	// server.AddHandlerFunc("/GetChain", GetChainHandler)
-	// MainChain.PushTransaction("a", "b", 2, "")
-	// MainChain.Mine("mxh")
-	// MainChain.PushTransaction("a", "c", 2, "")
-	// MainChain.Mine("mxh")
-	// MainChain.Print()
-	// server.Run()
-	wA := blockchain.NewWallet()
-	wB := blockchain.NewWallet()
-	wM := blockchain.NewWallet()
-	transaction, sign := wA.NewSignedTransaction(wA.GetAddr(), wB.GetAddr(), 1, "test")
-	bc := blockchain.NewBlockChain()
-	bc.PushTransaction(blockchain.MainNetAddr, wA.GetAddr(), 2, "")
-	bc.Mine(wA.GetAddr())
-	bc.ValidAndPushTransaction(wA.GetPublicKey(), *sign, *transaction)
-	fmt.Println("mining...")
-	bc.Mine(wM.GetAddr())
-	bc.Print()
-	fmt.Println("A:", bc.GetTotalValue(wA.GetAddr()))
-	fmt.Println("B:", bc.GetTotalValue(wB.GetAddr()))
-	fmt.Println("M:", bc.GetTotalValue(wM.GetAddr()))
+	server := chainserver.NewServer("localhost:", 8088)
+	server.AddHandlerFunc("/TestAlive", chainserver.TestAliveHandler)
+	server.AddHandlerFunc("/GetChain", chainserver.GetChainHandler)
+	chainserver.MainChain.PushTransaction("a", "b", 2, "")
+	chainserver.MainChain.Mine("mxh")
+	chainserver.MainChain.PushTransaction("a", "c", 2, "")
+	chainserver.MainChain.Mine("mxh")
+	server.Run()
+	// wA := blockchain.NewWallet()
+	// wB := blockchain.NewWallet()
+	// wM := blockchain.NewWallet()
+	// transaction, sign := wA.NewSignedTransaction(wA.GetAddr(), wB.GetAddr(), 1, "test")
+	// bc := blockchain.NewBlockChain()
+	// bc.PushTransaction(blockchain.MainNetAddr, wA.GetAddr(), 2, "")
+	// bc.Mine(wA.GetAddr())
+	// bc.ValidAndPushTransaction(wA.GetPublicKey(), *sign, *transaction)
+	// fmt.Println("mining...")
+	// bc.Mine(wM.GetAddr())
+	// bc.Print()
+	// fmt.Println("A:", bc.GetTotalValue(wA.GetAddr()))
+	// fmt.Println("B:", bc.GetTotalValue(wB.GetAddr()))
+	// fmt.Println("M:", bc.GetTotalValue(wM.GetAddr()))
 }
